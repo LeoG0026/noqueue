b12872ccd344745b5f00b074c88c3b5ef9976081
Bobby Williams <bobbywilliams@apps.ipb.ac.id>
2021-06-12 12:11:58 +0700

    change title

--- profile.php
+++ profile.php
@@ -16,7 +16,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
-    <title>Profile</title>
+    <title>Noqueue</title>
   </head>
   <body>
     <header class="header" style="background-image: url(./admin/images/cover-resto.jpeg)">
